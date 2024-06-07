2611961e241409ff27186df752c14b694a50174d
Joaquin linares <linaresjoaquin9@gmail.com>
2024-06-06 21:23:56 -0300

    actualizacion de portafolio

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-let menuVisible=!1;function mostrarOcultarMenu(){menuVisible?(document.getElementById("nav").classList="",menuVisible=!1):(document.getElementById("nav").classList="responsive",menuVisible=!0)}function seleccionar(){document.getElementById("nav").classList="",menuVisible=!1}function efectoHabilidades(){var s=document.getElementById("skills");if(window.innerHeight-s.getBoundingClientRect().top>=300){let e=document.getElementsByClassName("progreso");e[0].classList.add("javascript"),e[1].classList.add("htmlcss"),e[2].classList.add("photoshop"),e[3].classList.add("wordpress"),e[4].classList.add("drupal"),e[5].classList.add("comunicacion"),e[6].classList.add("trabajo"),e[7].classList.add("creatividad"),e[8].classList.add("dedicacion"),e[9].classList.add("proyect")}}window.onscroll=function(){efectoHabilidades()};
+let menuVisible = false;
+
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar() {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectoHabilidades() {
+    var s = document.getElementById("skills");
+    if (window.innerHeight - s.getBoundingClientRect().top >= 300) {
+        let e = document.getElementsByClassName("progreso");
+        e[0].classList.add("javascript");
+        e[1].classList.add("htmlcss");
+        e[2].classList.add("photoshop");
+        e[3].classList.add("wordpress");
+        e[4].classList.add("drupal");
+        e[5].classList.add("comunicacion");
+        e[6].classList.add("trabajo");
+        e[7].classList.add("creatividad");
+        e[8].classList.add("dedicacion");
+        e[9].classList.add("proyect");
+    }
+}
+
+window.onscroll = function() {
+    efectoHabilidades();
+};
